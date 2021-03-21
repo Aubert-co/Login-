604e77b122c9f84e76164d8ad41316a4f7d194a9
Black <53127242+Aubert-co@users.noreply.github.com>
2021-03-20 22:30:38 -0300

    Update login.js

--- route/login.js
+++ route/login.js
@@ -2,30 +2,46 @@ const route = require('express').Router()
 
 const db = require('../model/db')
 const bcrypt = require('bcrypt')
-const jwt = require('jws')
+const jwt = require('jsonwebtoken')
+const users = require('../middle/users')
 
 
-route.get('/login',(req,res)=>{
 
-    res.send('ola')
-})
+
 
 
 route.post('/login',(req,res)=>{
-    const {id,name,pass} = req.body
+    const {name,password} = req.body
+
+    
+  
     const data = `SELECT * FROM Login_Users WHERE name = '${name}'`
 
     db.query(data,(err,results)=>{
         if(err)throw err
 
         if(results.length === 0 )return res.send({err:'user not found'})
-        const {senha} = results[0]
-        const compare = bcrypt.compareSync(pass,senha)
+        const {senha,id} = results[0]
+  
+        const compare = bcrypt.compareSync(password,senha)
 
+        
         if(!compare)return res.send({err:'user not found'})
         
-        return res.json({sucess:'welcome'})
+        const tokens = {name,id}
+    
+        const token = jwt.sign({jwt:tokens},'segredo',{expiresIn:24*60*60})
+       
+        users.add({name,token})
+     
+        
+        
+    
+        return res.send({token,name})
+        
     })
+
+
 })
 
 module.exports = route
