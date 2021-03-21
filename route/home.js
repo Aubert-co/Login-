04970e181c307faf44e718c7f488f39ad288f290
Black <53127242+Aubert-co@users.noreply.github.com>
2021-03-20 22:29:04 -0300

    Create home.js

---
+++ route/home.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const user = require('../middle/Users')
+
+const jwt  = require('jws')
+
+
+router.get('/auth/home',(req,res)=>{
+
+
+
+ // res.send({msg:'home welcome'})
+})
+module.exports = router
