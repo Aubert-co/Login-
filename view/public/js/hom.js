08a0a665ca80ed8406f704934312ca692c356683
Black <53127242+Aubert-co@users.noreply.github.com>
2021-03-21 21:10:43 -0300

    Update hom.js

--- view/public/js/hom.js
+++ view/public/js/hom.js
@@ -1,12 +1,55 @@
 const token = localStorage.getItem('token')
+const DivAuth = document.querySelector('.auth')
+const logout = document.querySelector('.logout')
 
-fetch('http://localhost:8080/auth/home',{
-    headers:{
-        'x-api-token':'token'
+
+const get = async()=>{
+try{
+    const response = await fetch('http://192.168.100.54:8080/auth/home',{headers:{'x-api-token':token}})
+
+    const {msg,status} = await response.json()
+
+    if(status === 401){
+        window.location.href = 'login.html'
+        DivAuth.innerHTML = msg
     }
-})
-    .then((res)=>{
-        if(res.status===401)return console.log('error')
+    }catch(err){
+        if(err)throw err
+    }
+}
+get()
 
-       return res.json()
-    })
+/*
+function get (){
+    fetch('http://192.168.100.54:8080/auth/home',{headers:{'x-api-token':'token'}})
+        .then((res)=>res.json())
+        .then((datas)=>{
+            const {msg,status} = datas
+            alert(status)
+            if(status === 401){
+                alert('proibida')
+                window.location.href = 'login.html'
+                DivAuth.innerHTML = msg
+            }
+        })
+        .catch((err)=>{
+            if(err)console.log(err)
+        })
+}
+get()
+*/
+const logoutFunc = async()=>{
+    try{
+        const response = await fetch('http://192.168.100.54:8080/logout',{headers:{'x-api-token':token}}) 
+
+        const {msg,status} = await response.json()
+
+        const removToken = localStorage.removeItem('token')
+        
+        window.location.href = 'login.html'
+    }catch(err){
+        if(err)throw err
+    }
+}
+
+logout.addEventListener('click',logoutFunc)
