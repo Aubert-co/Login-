e56797fa3bee6d72492dd0768da116bcd6971e3f
Black <53127242+Aubert-co@users.noreply.github.com>
2021-03-22 23:34:36 -0300

    Update cadastro.js

--- route/cadastro.js
+++ route/cadastro.js
@@ -6,27 +6,27 @@ route
 
 .post('/cadastro',(req,res)=>{
     const {name,pass} = req.body
-  
+
     const sql = `SELECT * FROM Login_Users WHERE name='${name}'`
 
     db.query(sql,(err,results)=>{
         if(err)throw err
 
-
+     
         if(results.length ===0){
-            console.log('aw')
+           
             const senha = bcrypt.hashSync(pass,5)
             const save = `INSERT INTO Login_Users(name,senha) VALUES('${name}','${senha}')`
 
             db.query(save,(err,results)=>{
                 if(err)throw err
                 
-               return res.status(200).send('cadastrado com sucesso')
+               return res.send({status:200,msg:'registered successfully'})
             })
-
-        }else{
-            res.status(200).send('user ja cadastrado')
         }
+            
+            res.send({msg:'user already exist',status:201})
+        
     })
 })
 
