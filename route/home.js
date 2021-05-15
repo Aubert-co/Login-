acd1e05e395ca0950bfcb4c2b3f06361b9befb17
Black <53127242+Aubert-co@users.noreply.github.com>
2021-05-14 23:19:25 -0300

    Update home.js

--- route/home.js
+++ route/home.js
@@ -1,12 +1,10 @@
 const router = require('express').Router()
-const user = require('../middle/Users')
 
-const jwt  = require('jws')
 
 
 router.get('/auth/home',(req,res)=>{
 
 
-  res.send({msg:'home welcome'})
+  res.send({msg:'Welcome',status:200})
 })
 module.exports = router
