3d445726b05afa264c4144320dc16671c6b179b2
Black <53127242+Aubert-co@users.noreply.github.com>
2021-07-11 00:46:29 -0300

    Update mid.js

--- middle/mid.js
+++ middle/mid.js
@@ -3,12 +3,12 @@ const jwt  = require('jsonwebtoken')
 module.exports = (req,res,next)=>{
   const token = req.headers['x-api-token']
 
-    if (!token) return res.status(401).json({status:401,msg: 'No token provided.' });
+    if (!token) return res.status(401).json({msg: 'No token provided.' });
         
 
     jwt.verify(token,'segredo',function(err, decoded) {
       
-      if (err) return res.status(401).send({status:401,msg:'Not authorized'})
+      if (err) return res.status(401).send({msg:'Not authorized'})
       const {jwt} = decoded
       
       // se tudo estiver ok, salva no request para uso posterior
