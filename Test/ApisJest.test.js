0ddbfeab7cf49b060fb125203ba9b2d7e286f7ee
Black <53127242+Aubert-co@users.noreply.github.com>
2021-07-12 22:01:18 -0300

    Update ApisJest.test.js

--- Test/ApisJest.test.js
+++ Test/ApisJest.test.js
@@ -1,7 +1,9 @@
 const { expect } = require('chai')
 const request = require('supertest')
 ,app = require('../app')
+const dbSql = require('../model/db')
 var server
+,globalToken
 
 describe("POST /login",()=>{
     beforeAll((done)=>{
@@ -28,7 +30,71 @@ describe("POST /login",()=>{
     
         expect(resp.status).to.equal(200)
     })
+
+    describe("POST cadastro",()=>{
+        test("should return a status 405 when send wrong values",async()=>{
+            const resp = await request(server)
+            .post('/cadastro')
+            .set({'Content-Type':'application/json'})
+            .send({name:'',password:'123'})
+
+            expect(resp.status).to.equal(404)
+        })
+        test("should return a status 200 when send correct values",async()=>{
+            const resp = await request(server)
+            .post("/cadastro")
+            .set({'Content-Type':'application/json'})
+            .send({name:'mat',password:'123'})
+        })
+        beforeEach(async()=>{
+            const name = "mat"
+            const query = `DELETE FROM Login_Users WHERE name='${name}'` 
+
+            dbSql.query(query,(err,results)=>{
+                if(err)throw err
+            })
+
+            const SELECT = `SELECT * FROM Login_Users WHERE name='${name}'`
+            dbSql.query(SELECT,(err,results)=>{
+                if(err)throw err
+                expect(results.length).to.equal(0)
+            })
+
+            
+        })
+    })
+
+    describe("GET HOME,",()=>{
+        test("should return a status 404 when try logoin withou token",async()=>{
+            const resp = await request(server)
+            .get('/auth/home')
+            
+            expect(resp.status).to.equal(401)
+        })
+
+        beforeEach(async()=>{
+            const resp = await request(server)
+            .post('/login')
+            .set({'Content-Type':'application/json'})
+            .send({name:'jose',password:'123'})
+            
+            expect(resp.headers['x-api-token']).to.be.a('string')
+            
+            
+            globalToken =resp.headers['x-api-token']
+        })
+
+        test("should return a status 200 when try connect home with a token",async()=>{
+            const resp = await request(server)
+            .get('/auth/home')
+            .set({'x-api-token':globalToken})
+
+            expect(resp.status).to.equal(200)
+        })
+    })
     afterAll(async()=>{
         await server.close()
     })
 })
+
+
